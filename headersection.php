9fdebcd899a5ef02bd6e3bdec3813140b36ada5e
MaxPuddu <71008325+MaxPuddu@users.noreply.github.com>
2022-02-16 16:15:13 +0100

    Addition
    added a header page (first version)
    and the css file for the header page

---
+++ headersection.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once("./includes/meta.php");
+
+// php code hier
+
+ ?>
+
+ <!-- html code hier -->
+
+ <!DOCTYPE html>
+<html>
+	<head>
+		<title>Header</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <link rel="stylesheet" href="./includes/Assets/Style/headersection.css"> 
+	</head>
+
+	<body>
+        <header>
+            <div class="text-center">
+                <img src="./includes/Assets/Img/logo.png" alt="...">
+            </div>
+        </header>
+
+	</body>
+</html>
