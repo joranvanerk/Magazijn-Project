ba51c0a04714d4d842db25a20da5e0d028acac87
MaxPuddu <71008325+MaxPuddu@users.noreply.github.com>
2022-02-22 16:13:06 +0100

    update
    made a navbar with a search bar and an icon to the user page

--- headersection.php
+++ headersection.php
@@ -11,34 +11,30 @@ include_once("./includes/meta.php");
  <!DOCTYPE html>
 <html>
 	<head>
-		<title>Header</title>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width,initial-scale=1.0">
-        <link rel="stylesheet" href="./includes/Assets/Style/headersection.css">
-		<!-- link for search icon -->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
 	</head>
 
 	<body>
-        <header>
-			<div id = "boxes">
+		<!-- navbar color -->
+		<nav class="navbar navbar-light justify-content-between" style="background-color: #588caa;">
 
-				<div id = "leftbox">
-					<form class="example" action="action_page.php">
-						<input type="text" placeholder="Search.." name="search">
-						<button type="submit"><i class="fa fa-search"></i></button>
-					</form>
-				</div> 
-				
-				<div id = "middlebox">
-					<img src="./includes/Assets/Img/logo.png" alt="...">
-				</div>
-				
-				<div id = "rightbox">
-					<a href="..."><img src="./includes/Assets/Img/inlog.png" alt="..."></a>
-				</div>
-			</div>
-        </header>
+			<!-- search bar -->
+			<form class="responsivemb"  action="" method="post" autocomplete="off" style="margin-left: 20px">
+				<div class="input-group">
+					<div class="input-group-text" style="background-color: #FFFFFF; border: none; border-radius: 25px 0px 0px 25px; padding: 15px;"><i class="fa fa fa-search"></i></div>
+					<input placeholder="Zoeken.." style="border: none;
+					/* caret-color: transparent; */
+						font-size: 14px; border-radius: 0px 25px 25px 0px; padding: 15px; padding-right: 50px;" type="text" name="nav_num" autofocus>
+					</div>
+				<button style="opacity: 0%;" type="submit" name="nav_submit"></button>
+			</form>
 
+			<!-- user page icon -->
+			<a class="navbar-brand text-white" href="dashboard.php">Gebruikersnaam <i class="fa fa-user text-white fa-2xl"></i></a>
+
+		</nav>
+		
 	</body>
+	
 </html>
