60ad50d19213478a53fa15275930faf4fa9b927e
MaxPuddu <71008325+MaxPuddu@users.noreply.github.com>
2022-02-22 14:10:10 +0100

    update
    added images and a href to eventually link the medewerkers picture to the medewerkers page

--- headersection.php
+++ headersection.php
@@ -14,14 +14,30 @@ include_once("./includes/meta.php");
 		<title>Header</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width,initial-scale=1.0">
-        <link rel="stylesheet" href="./includes/Assets/Style/headersection.css"> 
+        <link rel="stylesheet" href="./includes/Assets/Style/headersection.css">
+		<!-- link for search icon -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
 	</head>
 
 	<body>
         <header>
-            <div class="text-center">
-                <img src="./includes/Assets/Img/logo.png" alt="...">
-            </div>
+			<div id = "boxes">
+
+				<div id = "leftbox">
+					<form class="example" action="action_page.php">
+						<input type="text" placeholder="Search.." name="search">
+						<button type="submit"><i class="fa fa-search"></i></button>
+					</form>
+				</div> 
+				
+				<div id = "middlebox">
+					<img src="./includes/Assets/Img/logo.png" alt="...">
+				</div>
+				
+				<div id = "rightbox">
+					<a href="..."><img src="./includes/Assets/Img/inlog.png" alt="..."></a>
+				</div>
+			</div>
         </header>
 
 	</body>
