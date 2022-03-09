b3174d8aad37e855eff34f8ce3abd367f08b69f2
Joranvanerk <joranvanerk1@gmail.com>
2022-03-09 15:55:52 +0100

    Update Createproduct.php
    Used a one page design for creating products

--- Createproduct.php
+++ Createproduct.php
@@ -1,12 +1,20 @@
 <?php
-include_once 'includes/db.php';
+include_once("./includes/meta.php");
 
-$name = $_POST['name'];
-$stock = $_POST["stock"];
+if(isset($_POST["submit"])){
+  // define variables for the query
+  $name = cleaning($_POST['name']);
+  $stock = cleaning($_POST["stock"]);
+  // execute query with cleaned variables
+  mysqli_query($conn, "INSERT INTO `stock` (`id`, `itemname`, `totalstock`, `totalavailability`)
+  VALUES (NULL, '$name', '$stock', '1')");
+}
 
-$sql =" INSERT INTO `stock` (`id`, `itemname`, `totalstock`, `totalavailability`)
-VALUES (NULL, '$name', '$stock', '1')";
-mysqli_query($conn, $sql);
-
-header("Location: ../index.php?product=succes");
 ?>
+
+<form action="" method="POST">
+    <input type="text" name="name" placeholder="Itemname">
+    <br>
+    <input type="text" name="stock" placeholder="Totalstock">
+    <button type="submit" name="submit">Confirm</button>
+</form>
