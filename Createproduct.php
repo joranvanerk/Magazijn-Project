66351c79c3d80cad298c727ae1fc96c213060096
PercyFalco <330187@student.mboutrecht.nl>
2022-03-09 15:49:24 +0100

    Create Product
    The page where you can add a new product to the stock is now working

---
+++ Createproduct.php
@@ -0,0 +1,12 @@
+<?php
+include_once 'includes/db.php';
+
+$name = $_POST['name'];
+$stock = $_POST["stock"];
+
+$sql =" INSERT INTO `stock` (`id`, `itemname`, `totalstock`, `totalavailability`)
+VALUES (NULL, '$name', '$stock', '1')";
+mysqli_query($conn, $sql);
+
+header("Location: ../index.php?product=succes");
+?>
