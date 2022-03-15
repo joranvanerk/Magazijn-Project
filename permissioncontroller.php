0131deef88deba88173d7ce8143f24d4c0a65a9a
maikel-dj <64279296+maikel-dj@users.noreply.github.com>
2022-03-15 13:54:59 +0100

    Create permissioncontroller.php
    Initial branch creation, still in progress..

---
+++ permissioncontroller.php
@@ -0,0 +1,34 @@
+<?php
+
+include_once("./includes/meta.php");
+
+$QUERY = "SELECT * FROM users";
+$results = mysqli_query($conn, $QUERY);
+$rows = mysqli_fetch_all($results, MYSQLI_ASSOC);
+
+
+
+
+ ?>
+
+ <!-- html code hier -->
+ <select name="user_id">
+     <?php
+     foreach($rows as $row) {
+
+        echo '<option value="'.$row["idusers"].'">'.$row["username"].'</option>';
+        
+    }
+
+     ?>
+</select>
+
+<select name="permission">
+  <option value="1">Student</option>
+  <option value="2">Warehouse Manager</option>
+  <option value="3">Financial Manager</option>
+  <option value="4">Superuser</option>
+</select>
+
+<button type="submit" name="submit">Edit Permission</button>
+
