add645a8ebf6a163b1556aa1efa40468ed8439d6
Joranvanerk <joranvanerk1@gmail.com>
2022-03-22 16:40:42 +0100

    Update dashboard.php
    Added two cards including the last news and the fast navigation card, to quickly navigate to pages.

--- dashboard.php
+++ dashboard.php
@@ -9,6 +9,29 @@ include_once("./includes/header.php");
  ?>
 
  <!-- hier je eigen code -->
- Dashboard pagina
+ <div style="margin: -10px; background-color: #588CAA;">
+   <h2 style="margin-left: 10px; color: white;">Welkom <?= $_SESSION["username"] ?> in het magazijn, want je magazijn!</h2>
+   <br>
+ </div>
+ <br>
+ <div class="card">
+  <div class="card-body text-center">
+    <h5 class="card-title">Laatste nieuws</h5>
+    <p class="card-text" style="color: #588CAA;">
+      Er is op dit moment geen nieuws
+    </p>
+  </div>
+</div>
+<br>
+ <div class="card">
+  <div class="card-body text-center">
+    <h5 class="card-title mb-3">Snel navigeren</h5>
+    <p class="card-text">
+      <a href="profile" class="btn" style="border-color: #588CAA; color: #588CAA;">Profiel</a>
+      <a href="requestnewproduct" class="btn" style="border-color: #588CAA; color: #588CAA;">Nieuw product aanvragen</a>
+      <a href="instellingen" class="btn" style="border-color: #588CAA; color: #588CAA;">Instellingen</a>
+    </p>
+  </div>
+</div>
 
  <?php include_once("./includes/footer.php"); ?>
