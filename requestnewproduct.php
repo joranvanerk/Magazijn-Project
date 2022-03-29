09efd98373c4c50b2b16cb47f9d9496fd46f9eea
maikel-dj <64279296+maikel-dj@users.noreply.github.com>
2022-03-29 20:50:11 +0200

    Update requestnewproduct.php
    One of the most final commits.
    Make sure everything in 'requests' table is not set to NOT NULL! or row will not insert.

--- requestnewproduct.php
+++ requestnewproduct.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 include_once("./includes/meta.php");
 include_once("./includes/sidebar.php");
@@ -13,17 +10,23 @@ $rows = mysqli_fetch_all($results, MYSQLI_ASSOC);
 
 if(isset($_POST["submit"])){
     // define variables for the query
-    $quanity = cleaning($_POST["quanity"]);
-    $userid = cleaning($_POST["id"]);
+    $usermail = cleaning($_POST["mailinput"]);
+    $itemid = cleaning($_POST["id"]);
+    $quantity = cleaning($_POST["quantity"]);
+    $description = cleaning($_POST["description"]);
+    //$expirationdate = date("Y-M-d", strtotime(date("Y-M-d")."+ 3 days"));
+    $expirationdate = date("Y-M-d");
+    $status = 0;
 
     // execute query with cleaned variables
     //Qmysqli_query($conn, "UPDATE `users` SET `permissions` = $permission WHERE `idusers` = $userid");
-    mysqli_query($conn, "INSERT INTO `requests` (`id`, `useremail`, `itemid`, `itemname`, `quanity`, `description`, `expirationdate`, `status`, `statusmessage`, `updateremail`) VALUES (1, 'info@maikel.nl', 600, 'Pen', 1, 'Ik wil schrijven', '29-03-2022', '0', 'Veel plezier', 'info@joranvanerk.nl')");
+    $query = "INSERT INTO `requests` ('usermail', 'itemid', 'quantity', 'description', 'expirationdate', 'status') VALUES ('$usermail', '$itemid', '$quantity', '$description', '$expirationdate', '$status') ";
+    mysqli_query($conn, "INSERT INTO `requests` (useremail, itemid, quantity, description, expirationdate, status, statusmessage, updateremail) VALUES ('$usermail', '$itemid', '$quantity', '$description', '$expirationdate', '$status', '', '') ");
   }
 
  ?>
 
-<form action="" method="POST">
+<form method="POST">
 
  <select name="id">
 
@@ -37,19 +40,19 @@ if(isset($_POST["submit"])){
      ?>
 </select>
 <br>
-<label for="quanity">Hoeveelheid:</label>
-<select name="quanity">
+<label for="quantity">Hoeveelheid:</label>
+<select name="quantity">
   <option value="1">1 Stuk.</option>
   <option value="2">2 Stuks.</option>
   <option value="3">3 stuks.</option>
 </select>
 <br>
 
-<label for="quanityinput">Beschrijving:</label>
-<input type="text" id="quanityinput" name="quanity"><br>
+<label for="description">Beschrijving:</label>
+<input type="text" id="description" name="description"><br>
 
 <label for="mailinput">Email:</label>
-<input type="text" id="mailinput" name="mail"><br>
+<input type="text" id="mailinput" name="mailinput"><br>
 
 
 
