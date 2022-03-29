23361747544f1f694daf9c12990116396bcf9775
Joranvanerk <joranvanerk1@gmail.com>
2022-03-29 22:05:13 +0200

    Create aanvragen.php
    Added "aanvragen" page for requests for the manager to accept of deny with a button.

---
+++ aanvragen.php
@@ -0,0 +1,85 @@
+<?php
+
+include_once("./includes/meta.php");
+include_once("./includes/sidebar.php");
+include_once("./includes/header.php");
+
+// get all requests
+$data_1 = "";
+$get_requests_query = mysqli_query($conn, "SELECT * FROM `requests` INNER JOIN users ON requests.useremail = users.mail WHERE `status`='0' ORDER BY `username` DESC LIMIT 100");
+while($requests_data = mysqli_fetch_assoc($get_requests_query)){
+  $data_1 .= '<tr>
+        <th scope="row">' . $requests_data["id"] . '</th>
+        <td>' . $requests_data["itemname"] . '</td>
+        <td>' . $requests_data["username"] . '</td>
+        <td>' . $requests_data["description"] . '</td>
+        <td>' . $requests_data["quantity"] . '</td>
+        <td>' . $requests_data["expirationdate"] . '</td>
+        <td><a class="btn btn-success"><i class="fas fa-check"></i></a> <a class="btn btn-danger"><i class="fas fa-times"></i></a></td>
+      </tr>';
+}
+
+// get requests that have been aproved
+$data_2 = "";
+$get_aproved_requests_query = mysqli_query($conn, "SELECT * FROM `requests` INNER JOIN users ON requests.useremail = users.mail WHERE `status`='1' ORDER BY `username` DESC LIMIT 20");
+while($requests_aproved_data = mysqli_fetch_assoc($get_aproved_requests_query)){
+  $data_2 .= '<tr>
+        <th scope="row">' . $requests_aproved_data["id"] . '</th>
+        <td>' . $requests_aproved_data["itemname"] . '</td>
+        <td>' . $requests_aproved_data["username"] . '</td>
+        <td>' . $requests_aproved_data["description"] . '</td>
+        <td>' . $requests_aproved_data["quantity"] . '</td>
+        <td>' . $requests_aproved_data["statusmessage"] . '</td>
+        <td><a class="btn btn-danger"><i class="fas fa-times"></i></a></td>
+      </tr>';
+}
+ ?>
+
+ <!-- card with all requests -->
+ <div class="card">
+  <div class="card-body">
+    <h2 class="text-center">Aanvragen nog te keuren</h2>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Object</th>
+      <th scope="col">Aangevraagd door</th>
+      <th scope="col">Opmerking</th>
+      <th scope="col">Aantal</th>
+      <th scope="col">Verloopdatum</th>
+      <th scope="col">Acties</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?= $data_1 ?>
+  </tbody>
+</table>
+  </div>
+</div>
+<br>
+
+<!-- card with all aproved requests -->
+<div class="card">
+ <div class="card-body">
+   <h2 class="text-center">Goedgekeurde aanvragen</h2>
+   <table class="table">
+ <thead>
+   <tr>
+     <th scope="col">#</th>
+     <th scope="col">Object</th>
+     <th scope="col">Aangevraagd door</th>
+     <th scope="col">Opmerking</th>
+     <th scope="col">Aantal</th>
+     <th scope="col">Status bericht</th>
+     <th scope="col">Acties</th>
+   </tr>
+ </thead>
+ <tbody>
+   <?= $data_2 ?>
+ </tbody>
+</table>
+ </div>
+</div>
+
+ <?php include_once("./includes/footer.php"); ?>
