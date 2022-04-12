a82605ac464664d021e30db8439c48a91b14629f
maikel-dj <64279296+maikel-dj@users.noreply.github.com>
2022-04-12 14:42:53 +0200

    Update profilepictures.php

--- profilepictures.php
+++ profilepictures.php
@@ -4,7 +4,36 @@ include_once("./includes/sidebar.php");
 include_once("./includes/header.php");
 ?>
 
+<!DOCTYPE html>
+<html>
+<body>
 
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
+
+<?php
+$target_dir = "profileimages/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+?>
 
 <?php include_once("./includes/footer.php"); ?>
 
