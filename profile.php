84ddf587a1948b17112c1f413ca8b54cf0855c0b
maikel-dj <64279296+maikel-dj@users.noreply.github.com>
2022-04-12 15:55:59 +0200

    Updated profile.php
    Translated project from english to dutch

--- profile.php
+++ profile.php
@@ -9,7 +9,7 @@ include_once("./includes/header.php");
 <body>
 
 <form action="" method="post" enctype="multipart/form-data">
-  Select image to upload:
+  Profielafbeelding bewerken:
   <input type="file" name="fileToUpload" id="fileToUpload">
   <input type="submit" value="Upload Image" name="submit">
 </form>
@@ -29,42 +29,42 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
+    echo "Bestand is een afbeelding - " . $check["mime"] . ".";
     $uploadOk = 1;
   } else {
-    echo "File is not an image.";
+    echo "Bestand is geen afbeelding.";
     $uploadOk = 0;
   }
 }
 
 // Check if file already exists
 if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
+  echo "Sorry, bestand bestaat al.";
   $uploadOk = 0;
 }
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
+  echo "Sorry, bestand is te groot.";
   $uploadOk = 0;
 }
 
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  echo "Sorry, we ondersteunen alleen bestanden met volgende datatypen: jpg, png, jpeg, gif.";
   $uploadOk = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
+  echo "Sorry, je bestand is niet geupload.";
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    echo "Het bestand ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " is geupload.";
   } else {
-    echo "Sorry, there was an error uploading your file.";
+    echo "Sorry, er was een error tijdens het uploaden van jouw bestand.";
   }
 }
 }
