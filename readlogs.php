8444fd4d1b5bb2e9ea3301e1a6cc82a535d40a88
MaxPuddu <71008325+MaxPuddu@users.noreply.github.com>
2022-05-24 16:14:23 +0200

    Update readlogs.php
    added documentation to the file to make the code easier to read and understand

--- readlogs.php
+++ readlogs.php
@@ -6,12 +6,13 @@ include_once("./includes/header.php");
 
 //to-do : delete logs
 
+//checks if the current user is a superuser
 $rol = $_SESSION["role"];
 if(!$rol == 0){
     echo '<meta http-equiv="refresh" content="0; URL=./dashboard">';
 }
 
-
+//the read function
 $finallogs = "";
 $get_logs_query = mysqli_query($conn, "SELECT * FROM `log` ORDER BY `id` desc LIMIT 50");
 while($logsdata = mysqli_fetch_assoc($get_logs_query)){
@@ -24,12 +25,13 @@ while($logsdata = mysqli_fetch_assoc($get_logs_query)){
 }
 
  ?>
-
+<!-- the card that shows the read function -->
  <div class="card">
   <div class="card-body">
     <h2 class="text-center">Meest recente veranderingen in de database.</h2>
     <table class="table table-striped">
   <thead>
+<!-- list that shows the items ordered -->
     <tr>
       <th scope="col" width="5%;">#</th>
       <th scope="col" width="45%;">Actie log</th>
